66727afcf0f2cec1ed0939befdaeabe57193aafa
VladimirZhd <zhd18001@byui.edu>
2020-11-14 16:21:36 -0700

    added try catch to controller

--- routes/prove/controllers/pr09.js
+++ routes/prove/controllers/pr09.js
@@ -13,66 +13,78 @@ const convertToJson = res => {
 };
 
 exports.getProve = async (req, res) => {
-  const data = await fetch(url).then(convertToJson);
-  if (data.previous) {
-    prevApi = data.previous;
-  } else {
-    prevApi = null;
+  try {
+    const data = await fetch(url).then(convertToJson);
+    if (data.previous) {
+      prevApi = data.previous;
+    } else {
+      prevApi = null;
+    }
+    if (data.next) {
+      nextApi = data.next;
+    } else {
+      nextApi = null;
+    }
+    res.render('pages/prove/pr09', {
+      title: 'Prove Week 9',
+      path: '/prove/pr09',
+      pokemons: data.results,
+      next: data.next,
+      prev: data.previous,
+    });
+  } catch (error) {
+    console.log(error);
   }
-  if (data.next) {
-    nextApi = data.next;
-  } else {
-    nextApi = null;
-  }
-  res.render('pages/prove/pr09', {
-    title: 'Prove Week 9',
-    path: '/prove/pr09',
-    pokemons: data.results,
-    next: data.next,
-    prev: data.previous,
-  });
 };
 
 exports.getNext = async (req, res) => {
-  const data = await fetch(nextApi).then(convertToJson);
-  if (data.previous) {
-    prevApi = data.previous;
-  } else {
-    prevApi = null;
+  try {
+    const data = await fetch(nextApi).then(convertToJson);
+    if (data.previous) {
+      prevApi = data.previous;
+    } else {
+      prevApi = null;
+    }
+    if (data.next) {
+      nextApi = data.next;
+    } else {
+      nextApi = null;
+      return res.redirect('/prove/pr09');
+    }
+    res.render('pages/prove/pr09', {
+      title: 'Prove Week 9',
+      path: '/prove/pr09',
+      pokemons: data.results,
+      next: data.next,
+      prev: data.previous,
+    });
+  } catch (error) {
+    console.log(error);
   }
-  if (data.next) {
-    nextApi = data.next;
-  } else {
-    nextApi = null;
-    return res.redirect('/prove/pr09');
-  }
-  res.render('pages/prove/pr09', {
-    title: 'Prove Week 9',
-    path: '/prove/pr09',
-    pokemons: data.results,
-    next: data.next,
-    prev: data.previous,
-  });
 };
 
 exports.getPrev = async (req, res) => {
-  const data = await fetch(prevApi).then(convertToJson);
-  if (data.previous) {
-    prevApi = data.previous;
-  } else {
-    prevApi = null;
-    return res.redirect('/prove/pr09');
+  try {
+    const data = await fetch(prevApi).then(convertToJson);
+    if (data.previous) {
+      prevApi = data.previous;
+    } else {
+      prevApi = null;
+      return res.redirect('/prove/pr09');
+    }
+    if (data.next) {
+      nextApi = data.next;
+    } else {
+      nextApi = null;
+    }
+    res.render('pages/prove/pr09', {
+      title: 'Prove Week 9',
+      path: '/prove/pr09',
+      pokemons: data.results,
+      next: data.next,
+      prev: data.previous,
+    });
+  } catch (error) {
+    console.log(error);
   }
-  if (data.next) {
-    nextApi = data.next;
-  } else {
-    nextApi = null;
-  }
-  res.render('pages/prove/pr09', {
-    title: 'Prove Week 9',
-    path: '/prove/pr09',
-    pokemons: data.results,
-    next: data.next,
-    prev: data.previous,
-  });
 };
